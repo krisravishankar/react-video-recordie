348967125bb3d4f243d6747c483f0781c72ccc52
Krishna Ravi Shankar <krisravishankar@gmail.com>
2021-06-12 07:37:22 +0530

    conditional rendering of buttons

--- src/components/video-recorder.component.tsx
+++ src/components/video-recorder.component.tsx
@@ -1,38 +1,72 @@
 import Button from '@material-ui/core/Button';
 import React, { useRef, useState } from 'react';
 import { useEffect } from 'react';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import { makeStyles } from '@material-ui/core/styles';
 import StopIcon from '@material-ui/icons/Stop';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
 import GetAppIcon from '@material-ui/icons/GetApp';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import VideocamOutlinedIcon from '@material-ui/icons/VideocamOutlined';
 
 export type VideoRecordPropType = {
   onRecord?: () => {};
   onStop?: () => {};
   onPlay?: () => {};
   onPause?: () => {};
+  onResume?: () => {};
   timeslice?: number;
+  filename?: string;
 };
+
+export enum VideoRecorderStateEnum {
+  initial,
+  ready,
+  recording,
+  inactive,
+  paused,
+  unsupported,
+}
 
 export function VideoRecorder({
   onRecord,
   onStop,
   onPause,
   onPlay,
+  onResume,
   timeslice,
+  filename,
 }: VideoRecordPropType) {
-  const video = useRef<HTMLVideoElement>(null);
-  const videoPlayer = useRef<HTMLVideoElement>(null);
+  const classes = useStyles();
+  const videoElement = useRef<HTMLVideoElement>(null);
   const chunks = useRef<Blob[]>([]);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
   const [videoUrl, setVideoUrl] = useState<string>('');
-  let mediaRecorder: MediaRecorder;
+  const [videoRecorderState, setVideoRecorderState] =
+    useState<VideoRecorderStateEnum>(VideoRecorderStateEnum.initial);
 
   const record = () => {
-    mediaRecorder.start(timeslice || 1000);
+    if (mediaRecorder) {
+      if (videoElement.current) {
+        videoElement.current.srcObject = mediaRecorder.stream;
+        videoElement.current.play();
+      }
+
+      mediaRecorder.start(timeslice || 1000);
+    }
+  };
+
+  const onMediaRecorderStart = () => {
+    setVideoRecorderState(VideoRecorderStateEnum.recording);
 
     if (onRecord) {
       onRecord();
+    }
+  };
+
+  const stop = () => {
+    if (mediaRecorder) {
+      mediaRecorder.stop();
     }
   };
 
@@ -41,32 +75,52 @@ export function VideoRecorder({
     const videoUrl = window.URL.createObjectURL(blob);
     setVideoUrl(videoUrl);
 
-    if (videoPlayer.current) {
-      videoPlayer.current.src = videoUrl;
+    if (videoElement.current) {
+      videoElement.current.pause();
+      videoElement.current.srcObject = null;
+      videoElement.current.src = videoUrl;
     }
 
     chunks.current = [];
+
+    setVideoRecorderState(VideoRecorderStateEnum.inactive);
 
     if (onStop) {
       onStop();
     }
   };
 
-  const stop = () => {
-    mediaRecorder.stop();
+  const pause = () => {
+    if (mediaRecorder) {
+      mediaRecorder.pause();
+    }
   };
 
-  const pause = () => {
-    mediaRecorder.pause();
+  const onMediaRecorderPause = () => {
+    setVideoRecorderState(VideoRecorderStateEnum.paused);
 
     if (onPause) {
       onPause();
     }
   };
 
+  const resume = () => {
+    if (mediaRecorder) {
+      mediaRecorder.resume();
+    }
+  };
+
+  const onMediaRecorderResume = () => {
+    setVideoRecorderState(VideoRecorderStateEnum.recording);
+
+    if (onResume) {
+      onResume();
+    }
+  };
+
   const play = () => {
-    if (videoPlayer && videoPlayer.current) {
-      videoPlayer.current.play();
+    if (videoElement && videoElement.current) {
+      videoElement.current.play();
     }
 
     if (onPlay) {
@@ -78,7 +132,7 @@ export function VideoRecorder({
     const a = document.createElement('a');
     a.style.display = 'none';
     a.href = videoUrl;
-    a.download = 'test.webm';
+    a.download = filename || 'videorecorder.webm';
     document.body.appendChild(a);
     a.click();
     setTimeout(() => {
@@ -96,14 +150,19 @@ export function VideoRecorder({
           video: true,
         })
         .then(function (stream) {
-          mediaRecorder = new MediaRecorder(stream);
+          const mediaRecorder = new MediaRecorder(stream);
+          mediaRecorder.onstart = onMediaRecorderStart;
           mediaRecorder.onstop = onMediaRecorderStop;
+          mediaRecorder.onpause = onMediaRecorderPause;
+          mediaRecorder.onresume = onMediaRecorderResume;
           mediaRecorder.ondataavailable = function (e) {
             chunks.current.push(e.data);
           };
-          if (video.current) {
-            video.current.srcObject = stream;
-            video.current.play();
+          setMediaRecorder(mediaRecorder);
+
+          if (videoElement.current) {
+            videoElement.current.srcObject = stream;
+            videoElement.current.play();
           }
         })
         .catch(function (err) {
@@ -116,45 +175,80 @@ export function VideoRecorder({
 
   return (
     <>
-      <video ref={video} playsInline autoPlay muted></video>
-      <div>
-        <Button
-          onClick={() => {
-            record();
-          }}
-        >
-          <FiberManualRecordIcon />
-        </Button>
-        <Button
-          onClick={() => {
-            stop();
-          }}
-        >
-          <StopIcon />
-        </Button>
-        <Button
-          onClick={() => {
-            pause();
-          }}
-        >
-          <PauseCircleFilledIcon />
-        </Button>
-        <Button
-          onClick={() => {
-            download();
-          }}
-        >
-          <GetAppIcon />
-        </Button>
-        <Button
-          onClick={() => {
-            play();
-          }}
-        >
-          <PlayCircleFilledIcon />
-        </Button>
+      <video
+        className={classes.videoRecorder}
+        ref={videoElement}
+        playsInline
+        autoPlay
+        muted
+      ></video>
+      <div className={classes.toolbar}>
+        {(videoRecorderState === VideoRecorderStateEnum.initial ||
+          videoRecorderState === VideoRecorderStateEnum.inactive) && (
+          <Button
+            onClick={() => {
+              record();
+            }}
+          >
+            <VideocamIcon />
+          </Button>
+        )}
+        {(videoRecorderState === VideoRecorderStateEnum.recording ||
+          videoRecorderState === VideoRecorderStateEnum.paused) && (
+          <Button
+            onClick={() => {
+              stop();
+            }}
+          >
+            <StopIcon />
+          </Button>
+        )}
+        {videoRecorderState === VideoRecorderStateEnum.recording && (
+          <Button
+            onClick={() => {
+              pause();
+            }}
+          >
+            <PauseCircleFilledIcon />
+          </Button>
+        )}
+        {videoRecorderState === VideoRecorderStateEnum.paused && (
+          <Button
+            onClick={() => {
+              resume();
+            }}
+          >
+            <VideocamOutlinedIcon />
+          </Button>
+        )}
+        {videoRecorderState === VideoRecorderStateEnum.inactive && (
+          <Button
+            onClick={() => {
+              download();
+            }}
+          >
+            <GetAppIcon />
+          </Button>
+        )}
+        {videoRecorderState === VideoRecorderStateEnum.inactive && (
+          <Button
+            onClick={() => {
+              play();
+            }}
+          >
+            <PlayCircleFilledIcon />
+          </Button>
+        )}
       </div>
-      <video ref={videoPlayer} playsInline muted></video>
     </>
   );
 }
+
+const useStyles = makeStyles({
+  videoRecorder: {
+    width: '100%',
+  },
+  toolbar: {
+    textAlign: 'center',
+  },
+});
